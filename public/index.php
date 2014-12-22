8229152f906f6c94576cf5e8aef7ac357405a8c8
Allan Kiezel <allan.kiezel@gmail.com>
2014-12-22 00:09:50 -0500

    Add root entry

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
 
 require __DIR__ . '/../bootstrap/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$input = $request->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
